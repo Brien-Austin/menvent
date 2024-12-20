27053cfcc787e03ed60c54ff4d53150c5d70a4ea
Brien-Austin <brienaustinclayton@gmail.com>
2024-12-21 01:20:09 +0530

    changed variable name

--- auth.ts
+++ auth.ts
@@ -5,7 +5,7 @@ import { prisma } from "@/lib/db";
 
 
 export const {handlers,signIn,signOut,auth}  = NextAuth({
-    secret : process.env.NEXT_PUBLIC_SECRET,
+    secret : process.env.NEXTAUTH_SECRET,
     adapter : PrismaAdapter(prisma),
     providers : [Google]
 })
