a598a389a83184e763286653fea3543ffa8802ba
Brien-Austin <brienaustinclayton@gmail.com>
2024-12-21 23:05:20 +0530

    logout feature

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -1,14 +1,23 @@
+"use client"
+
 import Back from '@/app/components/layout/back'
 import Hero from '@/app/components/layout/hero'
+import { LogOut } from 'lucide-react'
+import { signOut } from 'next-auth/react'
 
 import React from 'react'
 
 const Profile = () => {
   return (
-    <main className='p-8'>
+    <main className='p-8 w-full flex-1 '>
 
       <Hero text='Profile'/>
       <Back/>
+      <div onClick={()=>signOut()} className="mt-6 border w-full h-16 shadow-md rounded-lg flex p-3 items-center  gap-3 cursor-pointer">
+        <LogOut/> 
+        <h1>Logout</h1>
+
+      </div>
     </main>
   )
 }
--- src/app/components/layout/back.tsx
+++ src/app/components/layout/back.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Back = () => {
     const router = useRouter()
   return (
-    <div onClick={()=>{router.back()}} className="flex items-center gap-2 mt-4 border border-neutral-800 w-fit rounded-full px-3 py-2 hover:bg-neutral-900 cursor-pointer">
+    <div onClick={()=>{router.back()}} className="lg:flex sm:hidden items-center gap-2 mt-4 border border-neutral-800 w-fit rounded-full px-3 py-2 hover:bg-neutral-900 cursor-pointer">
     <ChevronLeft className='' size={18}/><h1 className='cursor-pointer'> Back</h1>
     </div>
   )
