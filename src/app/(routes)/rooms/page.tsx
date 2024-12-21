c1bb07c76da882ffae25d264ae57379ee8a62a32
Brien-Austin <brienaustinclayton@gmail.com>
2024-12-21 19:44:36 +0530

    add MobileNavBar component to Rooms page

--- src/app/(routes)/rooms/page.tsx
+++ src/app/(routes)/rooms/page.tsx
@@ -1,10 +1,12 @@
 import Back from '@/app/components/layout/back'
 import Hero from '@/app/components/layout/hero'
+import MobileNavBar from '@/app/components/layout/mobile-navbar'
 import React from 'react'
 
 const Rooms = () => {
   return (
     <main className='p-8'>
+      <MobileNavBar/>
       <Hero text='Rooms'/>
       <Back/>
     </main>
